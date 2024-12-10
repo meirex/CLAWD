7c7cac0bd9214118e8195ac12fb556a3f644c06c
meirex <meiraa1402@gmail.com>
2024-12-10 11:46:25 -0300

    deploy terceira vez

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -3,7 +3,6 @@ import { GameProps } from "@/utils/types/game";
 import Image from "next/image"
 import { FaStar, FaCalendar, FaGamepad, FaGlobe } from "react-icons/fa";
 
-
 // buscar dados do jogo com base no ID
 async function getData(id: string) {
   try {
@@ -21,18 +20,17 @@ async function getData(id: string) {
     console.error("failed to fetch data", err)
     return null
   }
-
 }
 
+interface PageProps {
+  params: { id: string }
+}
 
-
-export default async function Game({
-  params: { id } // Extrai o ID dos parâmetros da rota
-}: {
-  params: { id: string } // Define o formato esperado para os parâmetros
-}) {
+export default async function Game({ params }: PageProps) {
+  const { id } = params;
   // Chama a função de busca de dados e armazena o resultado
   const data = await getData(id);
+
   if (!data) {
     return (
       // Exibe uma tela de erro  ao usuário
@@ -47,9 +45,7 @@ export default async function Game({
     );
   }
 
-
   return (
-    
     <main className="w-full min-h-screen bg-gradient-to-br to-gray-900 from-gray-800 text-white">
       <div className="relative h-[50vh] w-full">
         {data.background_image && ( // Renderiza a imagem somente se ela existir
@@ -68,11 +64,9 @@ export default async function Game({
             {data.name}
           </h1>
           <div className="flex items-center space-x-4">
-
             <span className="bg-yellow-500 text-black px-3 py-1 rounded-full flex items-center">
               <FaStar className="mr-1" /> {data.rating.toFixed(1)}
             </span>
-
             <span className="bg-blue-500 px-3 py-1 rounded-full flex items-center">
               <FaCalendar className="mr-1" /> {data.released || 'N/A'}
             </span>
@@ -128,3 +122,4 @@ export default async function Game({
     </main>
   );
 }
+
